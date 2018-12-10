0728fbfbeb609466107da8c4fa53d3c16e2018ff
hzxgo <hezhixiongbei@126.com>
2018-12-10 17:56:48 +0800

    提供一个helloworld的接口

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func main() {
+	http.HandleFunc("/", hello)
+
+	if err := http.ListenAndServe(":8089", nil); err != nil {
+		fmt.Println("server error: ", err)
+	}
+}
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "text/plain")
+	fmt.Fprintf(w, "hello world.\nthe time is: %s", time.Now().Format("2006-01-02 15:04:05"))
+}
